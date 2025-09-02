8063a5fee87cc25de8693bd5897cec80397026fb
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-09-02 11:19:39 +0900

    feat: 앱 컬러 상수 추가

--- lib/shared/constants/app_colors.dart
+++ lib/shared/constants/app_colors.dart
@@ -2,15 +2,25 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // 브랜드/팔레트 토큰
-  static const primary = Color(0xFF4F46E5);
-  static const secondary = Color(0xFF00BFA6);
+  static const plumu_white = Color(0xFFFFFFFF);
+  static const plumu_black = Color(0xFF000000);
+  static const plumu_gray_1 = Color(0xFFF3F3F3);
+  static const plumu_gray_2 = Color(0xFFDEDEDE);
+  static const plumu_gray_3 = Color(0xFFDBDDE1);
+  static const plumu_gray_4 = Color(0xFFC5C8CF);
+  static const plumu_gray_5 = Color(0xFF9FA2AA);
+  static const plumu_gray_6 = Color(0xFF80818B);
+  static const plumu_gray_7 = Color(0xFF585A68);
+  static const plumu_gray_8 = Color(0xFF35353F);
 
-  static const neutral0  = Color(0xFFFFFFFF);
-  static const neutral10 = Color(0xFFF6F7FB);
-  static const neutral90 = Color(0xFF1C1C1E);
+
+  static const plumu_green_main = Color(0xFF5CBD56);
+  static const plumu_green_50per = Color(0x805CBD56);
+  static const plumu_green_30per = Color(0x4D5CBD56);
+
 
   // 상태 컬러
-  static const success = Color(0xFF10B981);
-  static const warning = Color(0xFFF59E0B);
-  static const danger  = Color(0xFFEF4444);
+  // static const success = Color(0xFF);
+  // static const warning = Color(0xFF);
+  // static const danger  = Color(0xFF);
 }
