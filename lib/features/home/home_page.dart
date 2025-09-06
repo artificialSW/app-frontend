420cac93df3e1e4452da0fcc076d1e9059389615
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-09-06 16:42:03 +0900

    feat: 홈페이지 대충 구현. 이제 로직 짜야함

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:artificialsw_frontend/shared/constants/app_assets.dart';
+import 'package:artificialsw_frontend/shared/constants/app_colors.dart';
+import 'package:artificialsw_frontend/shared/widgets/custom_button.dart';
 import 'package:artificialsw_frontend/shared/widgets/custom_top_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -7,28 +10,72 @@ class HomeRoot extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HomeTopBar(),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (_) => const DetailPage(title: 'Home detail')),
-            );
-          },
-          child: const Text('Go detail'),
-        ),
-      ),
-    );
-  }
-}
+      body: Column(
+        children: [
+          Container(
+            height: 30,
+            child: LinearProgressIndicator(value: 0.5, semanticsLabel: 'ddd', semanticsValue: 'dafasd'),
+          ),
+          SizedBox(height: 15),
+          Container(
+            height: 30,
+            child: LinearProgressIndicator(value: 0.5, semanticsLabel: 'ddd', semanticsValue: 'dafasd'),
+          ),
+          SizedBox(height: 15),
 
-class DetailPage extends StatelessWidget {
-  const DetailPage({super.key, required this.title});
-  final String title;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: const Center(child: Text('detail')),
+          //말풍선
+          Stack(
+            children: [
+              DecoratedBox(
+                  decoration: BoxDecoration(color: AppColors.plumu_green_main),
+                  child: Text('간단한 메세지를 남겨봐요!')
+              ),
+            ]
+          ),
+          SizedBox(height: 30),
+          SizedBox(
+            width: 350, height: 350,
+            child: Stack(
+              children: [
+                Positioned(
+                    top: 0,
+                    left: 20,
+                    child: Image.asset(
+                      AppAssets.tree,
+                      width: 300,
+                      height: 300,
+                      fit: BoxFit.contain,
+                    )
+                ),
+                Positioned(
+                    top: 200,
+                    left: 250,
+                    child: Image.asset(AppAssets.wooden_sign)
+                )
+              ],
+            ),
+          ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+              color: AppColors.plumu_gray_4,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Column(
+                children: [
+                  Container(
+                    height: 20,
+                    child: LinearProgressIndicator(value: 0.5, semanticsLabel: 'ddd', semanticsValue: 'dafasd'),
+                  ),
+                  SizedBox(height: 10),
+                  CustomButton(text: '나무 아카이브', onPressed: null)
+                ],
+              ),
+            )
+          )
+
+        ],
+      )
     );
   }
 }
