b27049080d3bbb8e42f4359d0d721e0c1e4a2b25
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-09-21 12:17:26 +0900

    feat: 진행중인 퍼즐 목록에 진행도 적용

--- lib/features/puzzle/puzzlelist_provider.dart
+++ lib/features/puzzle/puzzlelist_provider.dart
@@ -206,7 +206,7 @@ class PuzzleListItem extends StatelessWidget {
         Text('ai선정 키워드: ${puzzleDto.AIKeyword}',
             style: const TextStyle(color: Colors.grey, fontSize: 12)),
         const SizedBox(height: 8),
-        Text('진행도: ~~',
+        Text('진행도: ${(puzzleDto.completedPiecesCount/puzzleDto.size*100).toDouble().toStringAsFixed(0)}%',
             style: TextStyle(color: Colors.grey, fontSize: 12)),
         const SizedBox(height: 12),
         Row(
