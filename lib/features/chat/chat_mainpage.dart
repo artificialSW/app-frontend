819d1a5db2d01234b94449e77ca86b508f13f4eb
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-09-23 10:50:57 +0900

    feature/chat 손보기 2

--- lib/features/chat/chat_mainpage.dart
+++ lib/features/chat/chat_mainpage.dart
@@ -11,14 +11,6 @@ import 'widget/weekly_question_banner.dart';
 import 'widget/tab_bar.dart';
 import 'widget/custom_app_bar.dart';
 
-// 카드 목록 전용(페이지 내부 전용이므로 private)
-class _PersonalListItem {
-  final PersonalQuestionEntity entity;
-  final int likes;
-  final int comments;
-  const _PersonalListItem({required this.entity, required this.likes, required this.comments});
-}
-
 class ChatRoot extends StatefulWidget {
   const ChatRoot({super.key});
   @override
@@ -151,5 +143,13 @@ class _ChatRootState extends State<ChatRoot> {
       },
     );
   }
+}
+
+// 카드 목록 전용(페이지 내부 전용이므로 private)
+class _PersonalListItem {
+  final PersonalQuestionEntity entity;
+  final int likes;
+  final int comments;
+  const _PersonalListItem({required this.entity, required this.likes, required this.comments});
 }
 
