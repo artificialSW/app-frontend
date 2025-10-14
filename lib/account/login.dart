2de7be54a61437296e09125af416018136def955
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-10-14 11:19:13 +0900

    feat: 메인페이지 api에 엑세스토큰 로직 추가

--- lib/account/login.dart
+++ lib/account/login.dart
@@ -220,7 +220,7 @@ class _LoginScreenState extends State<LoginScreen> {
         // 2. 토큰 저장
         //await StorageService.saveAccessToken(accessToken, refreshToken);
         await StorageService.saveAccessToken(accessToken);
-
+        print('발급받은 access Token: $accessToken');
         // 3. 유저 가치(Value) 정보 API 호출
         // final userInfoUri = Uri.parse('$baseUrl/users/value');
         // final userInfoResponse = await http.get(
