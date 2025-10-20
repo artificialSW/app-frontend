e7abf593c88188f4a7e4df19cca9951cb8acade2
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-10-20 15:29:46 +0900

    feat: 마이페이지 선 디자인

--- lib/shared/constants/app_assets.dart
+++ lib/shared/constants/app_assets.dart
@@ -114,5 +114,8 @@ class AppAssets {
   static const String show_replies = 'assets/icons/show_replies.png';
 
   static const String check = 'assets/icons/check.png';
+  static const String reply_logo = 'assets/icons/reply_logo.png';
+  static const String heart_logo = 'assets/icons/heart_logo.png';
+  static const String puzzle_logo = 'assets/icons/puzzle_logo.png';
 
 }
