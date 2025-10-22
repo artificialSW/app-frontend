be3fd2bbb013270cbfec79982d282bf32b3402da
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-10-22 18:05:12 +0900

    준혁이 폰 맞춰서 부착물 달리는 위치 수정

--- lib/features/home/island_archive_logic/island_archive_page.dart
+++ lib/features/home/island_archive_logic/island_archive_page.dart
@@ -369,8 +369,8 @@ class _IslandArchivePageState extends State<IslandArchivePage> {
     size = isAcacia ? size * 1.6 : size; // 아카시아만 크기 증가
 
     final basePos = _baseFlowerPositions[index];
-    double left = basePos.dx * scaleX + 55;
-    double top = basePos.dy * scaleY + 308;
+    double left = basePos.dx * scaleX + 56;
+    double top = basePos.dy * scaleY + 316;
 
     //아카시아라면 위치를 살짝 위·왼쪽으로 이동 (-10, -10)
     if (isAcacia) {
@@ -403,8 +403,8 @@ class _IslandArchivePageState extends State<IslandArchivePage> {
     size = isAcacia ? size * 1.6 : size; // 아카시아만 크기 증가
 
     final basePos = _baseFlower2Positions[index];
-    double left = basePos.dx * scaleX + 124;
-    double top = basePos.dy * scaleY + 289;
+    double left = basePos.dx * scaleX + 129;
+    double top = basePos.dy * scaleY + 300;
 
     // 아카시아라면 위치를 살짝 위·왼쪽으로 이동 (-10, -10)
     if (isAcacia) {
@@ -438,8 +438,8 @@ class _IslandArchivePageState extends State<IslandArchivePage> {
     height = hasToShrink ? height * 0.85 : height;
 
     final basePos = _baseFruit1Positions[index];
-    final left = basePos.dx * scaleX + 189;
-    final top = basePos.dy * scaleY + 329;
+    final left = basePos.dx * scaleX + 195;
+    final top = basePos.dy * scaleY + 338;
 
     return Positioned(
       left: left,
@@ -464,8 +464,8 @@ class _IslandArchivePageState extends State<IslandArchivePage> {
     final height = 95.0 * scaleY;
 
     final basePos = _baseFruit2Positions[index];
-    final left = basePos.dx * scaleX + 250;
-    final top = basePos.dy * scaleY + 407;
+    final left = basePos.dx * scaleX + 258;
+    final top = basePos.dy * scaleY + 420;
 
     return Positioned(
       left: left,
